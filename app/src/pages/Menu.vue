7cbcc33bb90bd0ebfda0f826369cc77bffe8d7c4
Jose Monagas <josetmonagas97@gmail.com>
2019-11-13 17:55:30 -0300

    Finalizar paso 2: Carga de datos desde Excel a JSON

--- app/src/pages/Menu.vue
+++ app/src/pages/Menu.vue
@@ -27,9 +27,19 @@
                     <q-table
                       dense
                       :data="data"
-                      :columns="clasificados"
+                      :columns="[
+                      { name: 'cuenta', align: 'center', label: 'Nivel Plan de Cuenta', field: 'nivel', sortable: true },
+                      { name: 'cod', align: 'center', label: 'Codigo', field: 'cod', sortable: true },
+                      { name: 'desc', align: 'center', label: 'Descripcion', field: 'desc', sortable: true },
+                      { name: 'tipo', align: 'center', label: 'Tipo', field: 'tipo', sortable: true },
+                      { name: 'auxiliar', align: 'center', label: 'Auxiliar', field: 'auxiliar', sortable: true },
+                      { name: 'tipoIngreso', align: 'center', label: 'Tipo Ingreso/Costo', field: 'tipoIngreso', sortable: true },
+                      { name: 'imputable', align: 'center', label: 'Imputable', field: 'imputable', sortable: true },
+                      { name: 'negocio', align: 'center', label: 'Unidad de Negocios', field: 'negocio', sortable: true },
+                      { name: 'costo', align: 'center', label: 'Centro de Costos', field: 'costo', sortable: true }
+                      ]"
                       row-key="cod"
-                    />
+                      />
                   </q-card>
                 </q-dialog>
 
@@ -64,8 +74,13 @@
                       <q-card-section class="col">
                         <h4 class="text-center">Unidades de Negocios</h4>
                         <q-table
+                          dense
                           :data="unidadNegocios"
-                          :columns="columns"
+                          :columns="
+                          [
+                          { name: 'id', align: 'center', label: 'ID', field: 'id', sortable: true },
+                          { name: 'name', align: 'center', label: 'Nombre', field: 'nombre', sortable: true }
+                          ]"
                           row-keys="id"/>
                       </q-card-section>
                     </div>
@@ -104,8 +119,13 @@
                       <q-card-section class="col">
                         <h4 class="text-center">Centros de Costo</h4>
                         <q-table
+                          dense
                           :data="centroCostos"
-                          :columns="columns"
+                          :columns="
+                          [
+                          { name: 'id', align: 'center', label: 'ID', field: 'id', sortable: true },
+                          { name: 'name', align: 'center', label: 'Nombre', field: 'nombre', sortable: true }
+                          ]"
                           row-keys="id"/>
                       </q-card-section>
                     </div>
@@ -128,22 +148,9 @@
           >
           <q-btn-group spread>
             <q-btn class="q-ma-sm q-pa-sm" icon="fas fa-sign-in-alt" color="green" label="Ventas" stack @click="dVentas = true"/>
-            <q-btn class="q-ma-sm q-pa-sm" icon="fas fa-sign-out-alt" color="red" label="Compras" stack @click="dCompras = true"/>
-            <q-btn class="q-ma-sm q-pa-sm" icon="fas fa-receipt" color="orange" label="Boleta de Honorarios" stack  @click="dBoletas = true"/>
+              <q-btn class="q-ma-sm q-pa-sm" icon="fas fa-sign-out-alt" color="red" label="Compras" stack @click="dCompras = true"/>
+                <q-btn class="q-ma-sm q-pa-sm" icon="fas fa-receipt" color="orange" label="Boleta de Honorarios" stack  @click="dBoletas = true"/>
           </q-btn-group>
-
-          <q-dialog v-model="dVentas">
-            <q-card>
-              <q-card-section>
-                <q-input
-                  @input="val => { file = val[0] }"
-                  filled
-                  type="file"
-                  hint="Native file"
-                />
-              </q-card-section>
-            </q-card>
-          </q-dialog>
 
           <q-dialog v-model="dCompras">
             <q-card>
@@ -151,9 +158,63 @@
                 <q-input
                   @input="val => { file = val[0] }"
                   filled
+                  @change="reader($event, 2)"
                   type="file"
                   hint="Native file"
-                />
+                  />
+              </q-card-section>
+              <q-card-section>
+                <q-table
+                  dense
+                  :data="compras"
+                  :columns="[
+                  { name: 'nro', align: 'center', label: 'Nro', field: 'Nro', sortable: true },
+                  { name: 'tipoDoc', align: 'center', label: 'Tipo Doc', field: 'Tipo Doc', sortable: true },
+                  { name: 'rutProveedor', align: 'center', label: 'RUT Proveedor', field: 'RUT Proveedor', sortable: true },
+                  { name: 'razonSocial', align: 'center', label: 'Razon Social', field: 'Razon Social', sortable: true },
+                  { name: 'folio', align: 'center', label: 'Folio', field: 'Folio', sortable: true },
+                  { name: 'fechaDcto', align: 'center', label: 'Fecha Documento', field: 'Fecha Docto', sortable: true },
+                  { name: 'fechaRecp', align: 'center', label: 'Fecha Recepcion', field: 'Fecha Recepcion', sortable: true },
+                  { name: 'montoExento', align: 'center', label: 'Monto Exento', field: 'Monto Exento', sortable: true },
+                  { name: 'montoNeto', align: 'center', label: 'Monto Neto', field: 'Monto Neto', sortable: true },
+                  { name: 'montoTotal', align: 'center', label: 'Monto Total', field: 'Monto Total', sortable: true },
+                  ]"
+                  row-keys="nro"/>
+              </q-card-section>
+            </q-card>
+          </q-dialog>
+
+          <q-dialog v-model="dVentas">
+            <q-card>
+              <q-card-section>
+                <q-input
+                  @input="val => { file = val[0] }"
+                  filled
+                  @change="reader($event, 1)"
+                  type="file"
+                  hint="Native file"
+                  />
+              </q-card-section>
+              <q-card-section>
+                <q-table
+                  dense
+                  :data="ventas"
+                  :columns="[
+                  { name: 'nro', align: 'center', label: 'Nro', field: 'Nro', sortable: true },
+                  { name: 'tipoDoc', align: 'center', label: 'Tipo Doc', field: 'Tipo Doc', sortable: true },
+                  { name: 'tipoVenta', align: 'center', label: 'Tipo Venta', field: 'Tipo Venta', sortable: true },
+                  { name: 'rutCliente', align: 'center', label: 'RUT Cliente', field: 'Rut cliente', sortable: true },
+                  { name: 'razonSocial', align: 'center', label: 'Razon Social', field: 'Razon Social', sortable: true },
+                  { name: 'folio', align: 'center', label: 'Folio', field: 'Folio', sortable: true },
+                  { name: 'fechaDcto', align: 'center', label: 'Fecha Documento', field: 'Fecha Docto', sortable: true },
+                  { name: 'fechaRecp', align: 'center', label: 'Fecha Recepcion', field: 'Fecha Recepcion', sortable: true },
+                  { name: 'montoExento', align: 'center', label: 'Monto Exento', field: 'Monto Exento', sortable: true },
+                  { name: 'montoNeto', align: 'center', label: 'Monto Neto', field: 'Monto Neto', sortable: true },
+                  { name: 'montoIva', align: 'center', label: 'Monto IVA', field: 'Monto IVA', sortable: true },
+                  { name: 'montoTotal', align: 'center', label: 'Monto Total', field: 'Monto Total', sortable: true },
+                  { name: 'codigoSucursal', align: 'center', label: 'Codigo Sucursal', field: 'Codigo Sucursal', sortable: true },
+                  ]"
+                  row-keys="nro"/>
               </q-card-section>
             </q-card>
           </q-dialog>
@@ -161,12 +222,66 @@
           <q-dialog v-model="dBoletas">
             <q-card>
               <q-card-section>
-                <q-input
-                  @input="val => { file = val[0] }"
-                  filled
-                  type="file"
-                  hint="Native file"
-                />
+                <q-form
+                  @submit="onSubmit"
+                  @reset="onReset"
+                  class="q-gutter-md">
+                    <q-input
+                      filled
+                      v-model="nroBoleta"
+                      label="Numero"/>
+                    <q-input filled v-model="fechaBoleta" type="date" />
+                    <q-input
+                      filled
+                      v-model="estadoBoleta"
+                      label="Estado"/>
+                    <q-input
+                      filled
+                      v-model="rutBoleta"
+                      label="RUT Emisor"/>
+                    <q-input
+                      filled
+                      v-model="nombreBoleta"
+                      label="Nombre o Razon Social Emisor"/>
+                    <q-input
+                      filled
+                      v-model="socProfBoleta"
+                      label="Soc Prof"/>
+                    <q-input
+                      filled
+                      v-model="brutoBoleta"
+                      label="Honorario Bruto"/>
+                    <q-input
+                      filled
+                      v-model="retenidoBoleta"
+                      label="Honorario Retenido"/>
+                    <q-input
+                      filled
+                      v-model="pagadoBoleta"
+                      label="Honorario Pagado"/>
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+                </q-form>
+              </q-card-section>
+              <q-card-section>
+                <q-table
+                  dense
+                  :data="boletasHonorarios"
+                  :columns="[
+                  { name: 'nroBoleta', align: 'center', label: 'Numero', field: 'nro', sortable: true },
+                  { name: 'fechaBoleta', align: 'center', label: 'Fecha', field: 'fecha', sortable: true },
+                  { name: 'estadoBoleta', align: 'center', label: 'Estado', field: 'estado', sortable: true },
+                  { name: 'rutBoleta', align: 'center', label: 'RUT Emisor', field: 'rut', sortable: true },
+                  { name: 'nombreBoleta', align: 'center', label: 'Nombre Emisor', field: 'nombre', sortable: true },
+                  { name: 'socProfBoleta', align: 'center', label: 'Soc. Prof.', field: 'socProf', sortable: true },
+                  { name: 'brutoBoleta', align: 'center', label: 'Bruto', field: 'bruto', sortable: true },
+                  { name: 'retenidoBoleta', align: 'center', label: 'Retenido', field: 'retenido', sortable: true },
+                  { name: 'pagadoBoleta', align: 'center', label: 'Pagado', field: 'pagado', sortable: true },
+                  ]"
+                  row-key="nro"
+                  />
               </q-card-section>
             </q-card>
           </q-dialog>
@@ -214,6 +329,7 @@
 
 <script>
 import json from '../assets/clasificados.json'
+import XLSX from 'xlsx'
 
 export default {
   data () {
@@ -236,21 +352,18 @@ export default {
       nombreUnidadNegocio: null,
       centroCostos: [],
       unidadNegocios: [],
-      columns: [
-        { name: 'id', align: 'center', label: 'ID', field: 'id', sortable: true },
-        { name: 'name', align: 'center', label: 'Nombre', field: 'nombre', sortable: true }
-      ],
-      clasificados: [
-        { name: 'cuenta', align: 'center', label: 'Nivel Plan de Cuenta', field: 'nivel', sortable: true },
-        { name: 'cod', align: 'center', label: 'Codigo', field: 'cod', sortable: true },
-        { name: 'desc', align: 'center', label: 'Descripcion', field: 'desc', sortable: true },
-        { name: 'tipo', align: 'center', label: 'Tipo', field: 'tipo', sortable: true },
-        { name: 'auxiliar', align: 'center', label: 'Auxiliar', field: 'auxiliar', sortable: true },
-        { name: 'tipoIngreso', align: 'center', label: 'Tipo Ingreso/Costo', field: 'tipoIngreso', sortable: true },
-        { name: 'imputable', align: 'center', label: 'Imputable', field: 'imputable', sortable: true },
-        { name: 'negocio', align: 'center', label: 'Unidad de Negocios', field: 'negocio', sortable: true },
-        { name: 'costo', align: 'center', label: 'Centro de Costos', field: 'costo', sortable: true }
-      ]
+      ventas: [],
+      compras: [],
+      boletasHonorarios: [],
+      nroBoleta: null,
+      fechaBoleta: null,
+      estadoBoleta: null,
+      rutBoleta: null,
+      nombreBoleta: null,
+      socProfBoleta: null,
+      brutoBoleta: null,
+      retenidoBoleta: null,
+      pagadoBoleta: null
     }
   },
   computed: {
@@ -276,7 +389,22 @@ export default {
             'nombre': this.nombreCentroCosto
           }
         )
-        console.log(this.centroCostos)
+      }
+
+      if (this.idUnidadNegocio === null && this.idCentroCosto === null) {
+        this.boletasHonorarios.push(
+          {
+            'nro': this.nroBoleta,
+            'fecha': this.fechaBoleta,
+            'estado': this.estadoBoleta,
+            'rut': this.rutBoleta,
+            'nombre': this.nombreBoleta,
+            'socProf': this.socProfBoleta,
+            'bruto': this.brutoBoleta,
+            'retenido': this.retenidoBoleta,
+            'pagado': this.pagadoBoleta
+          }
+        )
       }
       this.onReset()
     },
@@ -285,13 +413,38 @@ export default {
       this.nombreCentroCosto = null
       this.idUnidadNegocio = null
       this.nombreUnidadNegocio = null
+      this.nroBoleta = null
+      this.fechaBoleta = null
+      this.estadoBoleta = null
+      this.rutBoleta = null
+      this.nombreBoleta = null
+      this.socProfBoleta = null
+      this.brutoBoleta = null
+      this.retenidoBoleta = null
+      this.pagadoBoleta = null
+    },
+    reader: function (evt, id) {
+      var reader = new FileReader()
+      var self = this
+      reader.onload = function (e) {
+        var data = new Uint8Array(e.target.result)
+        var workbook = XLSX.read(data, { type: 'array' })
+        var worksheet = workbook.Sheets[workbook.SheetNames[0]]
+        if (id === 1) {
+          self.ventas = XLSX.utils.sheet_to_json(worksheet)
+        }
+        if (id === 2) {
+          self.compras = XLSX.utils.sheet_to_json(worksheet)
+        }
+      }
+      reader.readAsArrayBuffer(evt.target.files[0])
     }
   }
 }
 </script>
 
 <style lang="scss">
-  .table-responsive {
-    overflow-y: hidden;
-  }
+.table-responsive {
+  overflow-y: hidden;
+}
 </style>
