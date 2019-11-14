ab5c85e531ea9c579706a98235104c3d64084c66
Jose Monagas <josetmonagas97@gmail.com>
2019-11-14 18:12:14 -0300

    Agregar diseño de formulario de inicio de sesion

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,9 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="full-width row wrap justify-end  content-center">
     <q-img
       src="statics/office.jpg"
       class="header-image absolute-top"
     />
+      <div class="q-pa-md q-mr-xl login-form shadow-3">
+        <q-form
+          @submit="onSubmit"
+          class="q-gutter-md">
+
+          <q-input
+            v-model="user"
+            label="Usuario"
+            standout="bg-primary text-white"
+            />
+
+          <q-input
+            v-model="password"
+            label="Contraseña"
+            type="password"
+            standout="bg-primary text-white"
+            />
+
+            <div>
+              <q-btn type="submit" label="Ingresar" color="primary" />
+            </div>
+
+        </q-form>
+
+        <a href="">¿Olvidaste tu clave?</a>
+        <a href="">¿No tienes una cuenta Rapido y Facil?</a>
+      </div>
+
   </q-page>
 </template>
 
@@ -19,4 +47,9 @@ export default {
   z-index: -1;
   opacity: 0.8;
 }
+
+.login-form {
+  background-color: rgba(255,255,255,0.8);
+}
+
 </style>
